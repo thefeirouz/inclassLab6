01de6bb8726d8320a10dcc42afd3c95ea97ac311
Eric Feirouz <efeirouz@u.rochester.edu>
2019-03-01 14:38:32 -0500

    s

---
+++ README.md
@@ -0,0 +1,15 @@
+# In Class Lab6
+
+## Contrast
+
+The number one glaring issue with the original website was the lack of contrast between the components of the websites. I decided to change the background to a light baby blue, and I contrasted that with a yellow. The darker the shade of yellow, the more important I felt the information was. As you scroll through the webpage, the contrast becomes less extreme.
+
+I also added a thick red border to the headings and paragraphs in order to grab the readers attention.
+
+
+##Readibility
+For readability, I added more padding to the text to increase the spacing between the borders. This made it less cluttered.
+
+## Typography
+
+The typefaces I chose were: Aldrich, IBM Plex Mono. I chose both of these fonts to portray the interest Leo had in technology. These fonts were futuristic and computer like. The headings, Aldrich, was a more extreme computer-like typeface. Plex Mono was used for the paragraphs because it was less extreme and more legible for long strings of words.
---
+++ index.php
@@ -0,0 +1,58 @@
+<?php include "inc/top.inc"; ?>
+<link href="https://fonts.googleapis.com/css?family=Aldrich|IBM+Plex+Mono|Press+Start+2P" rel="stylesheet">
+	<title>Leonard L Koblence | Lab 2: Flash Website</title>
+	<?php include "inc/links.inc"; ?>
+	<header>
+		<a href="index.php">
+		<h1>Leonard L Koblence</h1>
+		<img src="images/leo.jpg" alt="leo">
+	    </a>
+	</header>
+
+<aside>
+	<ul> 
+		<li>Tennis Player</li>
+		<li>Ok at CSS</li>
+		<Li>History Major, Computer Science Minor</Li>
+	</ul>
+</aside>
+
+<main>
+	<section class="design">
+		<article class="item">
+			<h2>Background</h2>
+			<p>My name is Leonard Koblence and I am from Miami, Florida. I spent most of my life travelling and living between both Miami and the New York City area. I have one younger sibling, and two parents who are European immigrants; France and Latvia respectively. I enjoyed athletic pursuits from an early age and by the time I reached middle school, I was fully committed to being a tennis player. I competed throughout middle school and high school in order to earn a national rank. </p>
+		</article>
+
+		<article>
+		<h2>Before College</h2>
+		<p>I developed a love for computers and technology which was reflected in some classes I took on IT in high school. I tried to learn about topics like networking, system administration, and how data centers work. After getting through with my SATs, I began applying to various colleges with the University of Rochester being a top pick of mine. After finding out that I got accepted to the school, and was recruited to the varsity tennis team, I hastily accepted the opportunity and began studying at the school soon after graduating from high school.</p>
+		</article>
+	</section>
+ 
+ <div class="bg">
+ 	<h2>School/Internships</h2>
+ 	<p> 
+	I switched to a more manageable major, being history, that more aligned with my military career aspirations while also giving me the freedom to pursue computer science as a minor. I ultimately put being on the tennis team on hold during my sophomore year in order to devote more time to military training. During my time at the University of Rochester I naturally taken many history courses that required lots of reading assignments and essay writings but were otherwise very intellectually rich and enjoyable.  In the beginning of my sophomore year I decided that I wanted to minor in computer science rather than major in it as I originally planned. My first computer science course was CSC 170 intro to web development where I learned how to make websites using html, css, php, jquery, and java script. I was fairly successful in the course and hope to continue to expand my knowledge of web development technologies in the advanced course on front end web development during my sophomore year. </p>
+
+	<p>Finally, I applied for a internship at the New York Internet company as a data center technician. I worked there for a while installing and testing switches and servers in the data center. Every so often I got to go into the network operations center and learn from the senior systems administrators how they did their jobs. 
+    </p>
+
+    </div>
+
+    <div class="bg1">
+ 	<h2>Interests</h2>
+ 	<p>
+ 	Outside of school work, I enjoy doing things on my own time. One of my hobbies is tennis, which I started playing at a young age. Additionally, I also have a love for computers and technology, which is reflected in my computer science minor. Outside of these activities, I like to relax and hang out with friends. I also enjoy traveling. The summer before going to college I was lucky enough to travel to some countries in Africa, mainly Morocco, and experience the rich and diverse culture there.
+ 	</p>
+ 	</div>
+ 	
+ 	<nav class="menu">
+		<ul>
+			<li><a href="http://csc174.org/assignment04/manhattan/leo.php">Click Here to See the Original Website</a></li>
+		</ul>
+	</nav>
+</main>
+
+
+
